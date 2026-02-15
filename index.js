44d30426448ac96b4d01b238315bbd32b861fe4a
vanshika sahu <vanshikasahu281105@gmail.com>
2026-02-15 19:17:00 +0530

    feature2

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const name = "John Doe";
 
 console.log(`My name is ${name} and I am ${age} years old.`);
 console.log("This is a simple JavaScript program.");
+
+const age1 = 25;
+const name1 = "John Doe";
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+console.log("This is a simple JavaScript program.");
